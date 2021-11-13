519359d651af7a66bd80e68e58871ba4a68e75f0
Ethan Mendel <ethanmendel516@gmail.com>
2021-11-13 16:06:28 -0500

    add main part oof Dijkstras (need relax still)

--- Project2.py
+++ Project2.py
@@ -1,10 +1,36 @@
+from numpy.core.numeric import Inf
 from numpy.lib.npyio import load
 import numpy as np
+import math
+import heapq
 
-def getKey(keys, k):
+class Node:
+    def __init__(self,i,source=False):
+        self.index = i
+        if(source):
+            self.d = 0
+        else:
+            self.d = math.inf
+        self.p = None
+    def __str__(self):
+        return f"{self.index} costs {self.d} from parent {self.p}"
+    def __eq__(self, o):
+        return self.d == o.d and self.index == o.index
+    def __gt__(self,o):
+        return self.d > o.d
+    def __lt__(self,o):
+        return self.d < o.d
+
+
+def getIndex(keys, k):
     if(not (k in keys)):
         keys.append(k)
     return keys.index(k)
+def getKey(keys,i):
+    if(i<len(keys)):
+        return keys[i]
+    else:
+        return None
 
 def loadGraph(fileName="sample.txt"):
     fileTxt = open(fileName, "r").read()
@@ -19,8 +45,8 @@ def loadGraph(fileName="sample.txt"):
     for l in lines:
         h = l.split(" ")
         if(len(h) == 3):
-            i1 = getKey(keys,h[0])
-            i2 = getKey(keys,h[1])
+            i1 = getIndex(keys,h[0])
+            i2 = getIndex(keys,h[1])
             graph[i1,i2] = h[2]
             if(undirected):
                 graph[i2,i1] = h[2]
@@ -28,7 +54,26 @@ def loadGraph(fileName="sample.txt"):
             startingNode = h[0]
     return (graph, keys, startingNode)
 
+def relax(graph):
+    pass
+
+def dijkstras(graph,keys,source):
+    #initialize-single-source
+    Q = []
+    for k in keys:
+        i = getIndex(keys,k)
+        if(k == source):
+            heapq.heappush(Q,Node(i,True))
+        else:
+            heapq.heappush(Q,Node(i))
+    S = []
+    while(len(Q) > 0):
+        u = heapq.heappop(Q)
+        S.append(u)
+        for k in keys:
+            v = getIndex(keys,k)
+            if(graph[u.index,v] > 0):
+                relax(graph,u.index,v)
 
 (graph,keys, startingNode) = loadGraph()
-print(keys)
-print(graph)
+dijkstras(graph,keys,startingNode)
